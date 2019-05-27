6b9de9b3c130d01124cb381cef4623ee406ce09c
takatoshi.donga <takatoshi.donga@rakus.co.jp>
2019-05-27 17:25:07 +0900

    Item Dao作成

---
+++ src/main/java/com/example/domain/Item.java
@@ -0,0 +1,32 @@
+package com.example.domain;
+
+public class Item {
+
+	/** 名前 */
+	private String name;
+
+	/** 価格 */
+	private Integer price;
+
+	@Override
+	public String toString() {
+		return "Item [name=" + name + ", price=" + price + "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+}
