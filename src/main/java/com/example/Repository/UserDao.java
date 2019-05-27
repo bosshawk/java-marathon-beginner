0d04efdfc9c56c55c268e7c163bf0aafe854c7f8
takatoshi.donga <takatoshi.donga@rakus.co.jp>
2019-05-27 16:54:25 +0900

    コントロールを作成

--- src/main/java/com/example/Repository/UserDao.java
+++ src/main/java/com/example/Repository/UserDao.java
@@ -2,7 +2,9 @@ package com.example.Repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.User;
@@ -22,6 +24,13 @@ public class UserDao {
 		user.setAddress(rs.getString("address"));
 		return user;
 	};
+	
+	
+	public User load(Integer id) {
+		String sql = "SELECT * FROM info WHERE id=:id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		return template.queryForObject(sql, param, USER_ROW_MAPPER);
+	}
 	
 	
 
--- src/main/java/com/example/controller/SearchController.java
+++ src/main/java/com/example/controller/SearchController.java
@@ -1,23 +1,47 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.Repository.UserDao;
+import com.example.domain.User;
 
 @Controller
 @RequestMapping("/search")
 public class SearchController {
 	
+	@Autowired
+	private UserDao userDao;
+	
+	/**
+	 * 入力画面へフォワード.
+	 * @return 入力画面へフォワード
+	 */
 	@RequestMapping("")
 	public String index() {
 		return "userinfosearch";
 	}
 	
+	/**
+	 * 入力されたidを主キー検索し、出力画面へ遷移.
+	 * 
+	 * @param id : 入力されたid
+	 * @param redirectAttributes : 出力ユーザー情報
+	 * @return 出力画面へ遷移
+	 */
 	@RequestMapping("/search")
-	public String search(String id) {
-		
+	public String search(String id,RedirectAttributes redirectAttributes) {
+		User user = userDao.load(Integer.parseInt(id));
+		redirectAttributes.addFlashAttribute("user", user);
 		return "redirect:/search/output";
 	}
 	
+	/**
+	 * 出力画面へ遷移.
+	 * @return 出力画面へ遷移
+	 */
 	@RequestMapping("/output")
 	public String output() {
 		return "userinfoview";
