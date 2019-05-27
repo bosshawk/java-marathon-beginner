010d4e6a010f004b8702ea7742a63e3788664cba
takatoshi.donga <takatoshi.donga@rakus.co.jp>
2019-05-27 14:20:52 +0900

    redirectAttributesを修正

--- src/main/java/com/example/controller/NameReceiveController.java
+++ src/main/java/com/example/controller/NameReceiveController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -13,13 +14,23 @@ public class NameReceiveController {
 		return "inputname";
 	}
 	
+	/**
+	 * @param name
+	 * @param redirectAttributes
+	 * @param model
+	 * @return 出力画面にリダイレクト
+	 */
 	@RequestMapping("/input")
-	public String input(String name,RedirectAttributes redirectAttributes) {
+	public String input(String name,RedirectAttributes redirectAttributes,Model model) {
 		
-		redirectAttributes.addAttribute("name", name);
+		redirectAttributes.addFlashAttribute("name", name);
 		return "redirect:/name/output";
 	}
 	
+	/**
+	 * 
+	 * @return 出力画面にフォワード
+	 */
 	@RequestMapping("/output")
 	public String output() {
 		return "outputname";
