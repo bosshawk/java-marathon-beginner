a53dd865953e88ee74624a8cede14d2249ffa81c
takatoshi.donga <takatoshi.donga@rakus.co.jp>
2019-05-27 14:50:38 +0900

    5km完成 出力画面完成 javadocコメントを書いた

--- src/main/java/com/example/controller/UserInfoController.java
+++ src/main/java/com/example/controller/UserInfoController.java
@@ -10,11 +10,24 @@ import com.example.domain.User;
 @RequestMapping("/userinfo")
 public class UserInfoController {
 
+	/**
+	 * 入力画面へフォワード
+	 * @return 入力画面へ
+	 */
 	@RequestMapping("")
 	public String index() {
 		return "inputuserinfo";
 	}
 	
+	/**
+	 * 入力データを取得し、フラッシュスコープに格納して出力へリダイレクト.
+	 * 
+	 * @param name : 入力名前
+	 * @param age : 入力年齢
+	 * @param address : 入力住所
+	 * @param redirectAttributes
+	 * @return 出力へリダイレクト
+	 */
 	@RequestMapping("/input")
 	public String input(
 			String name,String age,String address,
@@ -24,6 +37,10 @@ public class UserInfoController {
 		return "redirect:/userinfo/output";
 	}
 	
+	/**
+	 * 出力画面へフォワード.
+	 * @return 出力画面へ
+	 */
 	@RequestMapping("/output")
 	public String output() {
 		return "outputuserinfo";
