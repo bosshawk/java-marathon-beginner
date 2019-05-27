22af19b95f9cf3737d3141e210a0b7313bd5b9b2
takatoshi.donga <takatoshi.donga@rakus.co.jp>
2019-05-27 14:31:57 +0900

    Userのdomainを完成 Userのフィールド変数:name,age,address

--- src/main/java/com/example/controller/UserInfoController.java
+++ src/main/java/com/example/controller/UserInfoController.java
@@ -1,5 +1,14 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/userinfo")
 public class UserInfoController {
 
+	@RequestMapping("")
+	public String index() {
+		return "inputuserinfo";
+	}
 }
--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -1,5 +1,40 @@
 package com.example.domain;
 
 public class User {
+	/** 名前 */
+	private String name;
+	/** 年齢 */
+	private Integer age;
+	/** 住所 */
+	private String address;
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + ", address=" + address + "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 }
